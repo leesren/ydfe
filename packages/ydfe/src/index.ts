2139bc6a8b24dd402db98505ae27a9c477482aab
less zenberge <leesren@foxmail.com>
2020-03-29 22:05:21 +0800

    feat: d.ts

--- packages/ydfe/src/index.ts
+++ packages/ydfe/src/index.ts
@@ -6,4 +6,4 @@ export {
     deviceUtils,
     fileUtils,
     toolsUtil
-} 
+}
---
+++ packages/ydfe/src/type.d.ts
@@ -0,0 +1,16 @@
+
+
+export interface DeviceUtils {
+
+}
+export interface FileUtils {
+
+}
+export interface ToolsUtil {
+
+}
+export interface Props {
+    deviceUtils: DeviceUtils,
+    fileUtils: FileUtils,
+    toolsUtil: ToolsUtil
+}
